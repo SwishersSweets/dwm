6f1f63c776db1fb7a1fb93eae649863073864976
SwishersSweets <111619350+SwishersSweets@users.noreply.github.com>
2024-07-29 11:04:46 -0400

    initial commit

--- config.def.h
+++ config.def.h
@@ -1,24 +1,19 @@
 /* See LICENSE file for copyright and license details. */
 
+
+
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int gappx     = 4;        /* gaps between windows */
-static const unsigned int snap      = 2;       /* snap pixel */
-static const int swallowfloating = 0 ; /*1 means swallow floating window by default*/
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
-static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
-static const unsigned int systrayspacing = 2;   /* systray spacing */
-static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
-static const int showsystray        = 1;        /* 0 means no systray */
+static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const unsigned int snap      = 10;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "jetbrainsmono:size=10" };
-static const char dmenufont[]       = "jetbrainsmono:size=10";
-static const char col_gray1[]       = "#0e0e0e";
-static const char col_gray2[]       = "#000000";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#101010";
-static const char col_cyan[]        = "#bbbbbb";
+static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
+static const char col_gray1[]       = "#222222";/*background color */ 
+static const char col_gray2[]       = "#000000";/*inactive window border color */
+static const char col_gray3[]       = "#bbbbbb";/*font color */
+static const char col_gray4[]       = "#101010";/*top bar second color and active window border color */ 
+static const char col_cyan[]        = "#bbbbbb";/*active window color*/
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
@@ -33,11 +28,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",    NULL,     NULL,           0,         1,          0,           0,        -1 },
-	{ "Firefox", NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
-	{ "Alacritty",      NULL,     NULL,           0,         0,          1,           0,        -1 },
-	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
+	/* class      instance    title       tags mask     isfloating   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
@@ -63,33 +56,25 @@ static const Layout layouts[] = {
 
 /* helper for spawning shell commands in the pre dwm-5.0 fashion */
 #define SHCMD(cmd) { .v = (const char*[]){ "/bin/sh", "-c", cmd, NULL } }
-#define ss(cmd) { .v = (const char*[]){ "bin/sh", "-c", cmd, NULL } } 
+
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
-static const char *st[] = { "st" , NULL }; 
-static const char *sp[] = { "src", NULL };
-static const char *bm[] = { "/home/nate/.config/scripts/dmenubookmark",  NULL };
-static const char *rb[] = {"/home/nate/.config/scripts/dmenupower", NULL};
 
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_d,      spawn,          {.v = dmenucmd } },
-	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY|ShiftMask, 		        XK_Return, spawn, 	       {.v = st } },
-	{ MODKEY|ShiftMask,		          XK_s,	     spawn, 	       {.v = sp } },
-  { MODKEY|ShiftMask,             XK_b,      spawn,          {.v = bm } },
-  { MODKEY|ShiftMask,             XK_x,      spawn,          {.v = rb } },
-  { MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY|ShiftMask,             XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_k,      focusstack,     {.i = -1 } },
+	{ MODKEY,	                      XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_p,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_p,      incnmaster,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_a,      setmfact,       {.f = -0.05} },
-	{ MODKEY|ShiftMask,             XK_d,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Tab,    zoom,           {0} },
-	{ MODKEY|ShiftMask,             XK_Tab,    view,           {0} },
+	{ MODKEY|ShiftMask,             XK_d,      setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_a,      setmfact,       {.f = +0.05} },
+	{ MODKEY,             	      	XK_Tab,    zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Return,  view,           {0} },
 	{ MODKEY|ShiftMask,             XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
@@ -102,9 +87,6 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
@@ -121,8 +103,8 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
-	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
+	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
+	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
